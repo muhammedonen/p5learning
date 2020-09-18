14b2ada7e0aa827e16f00f0687b17601711815ad
muhammed80 <30301543+muhammed80@users.noreply.github.com>
2020-09-19 01:03:03 +0300

    button added

---
+++ main.js
@@ -0,0 +1,32 @@
+let w = 1220;
+let h = 450;
+let bcolor = "#4b5d67"
+let button;
+
+
+function setup() {
+  createCanvas(w, h);
+  background(bcolor);
+
+  button = createButton('TIKLA');
+  button.position(10,430);
+  button.size(100);
+  button.mousePressed(change);
+}
+
+function change() {
+  let colors = ['#62760c', '#206a5d', '#cf1b1b'];
+  let clr = random(colors);
+  background(clr);
+  
+}
+
+function draw() {
+  //point(100,50);
+  //strokeWeight(10);
+  //to be continued
+
+
+
+
+}
