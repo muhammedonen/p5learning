3577b925a76dbbde820f5e24c27cb7be44a795a8
muhammed80 <30301543+muhammed80@users.noreply.github.com>
2020-09-20 22:24:16 +0300

    xx

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let bcolor = "#4b5d67"
 let button;
 
 
+
 function setup() {
   createCanvas(w, h);
   background(bcolor);
@@ -20,14 +21,22 @@ function change() {
   let colors = ['#62760c', '#206a5d', '#cf1b1b'];
   let clr = random(colors);
   background(clr);
-  noStroke();
-  circle(100,100,20);
+  
+  
+
+  
 }
 
 function draw() {
-  //point(100,50);
-  //strokeWeight(10);
-  //to be continued
+  background(bcolor)
+let hr = hour();
+let mnt = minute();
+let scnd = second();
+
+fill('#ff414d');
+noStroke();
+textSize(50);
+text( hr + ':' + mnt + ':' + scnd , 610, 50);
 
 
 
